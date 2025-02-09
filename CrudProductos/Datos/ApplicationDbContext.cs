ed80385707b250a915638fbc0db7c54952a6a999
jose Angel <josegallardo.cor@gmail.com>
2025-02-08 19:58:24 -0600

    configuaracion de la cadena de conexion y ApplicationDbContext

---
+++ CrudProductos/Datos/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudProductos.Datos
+{
+    public class ApplicationDbContext: DbContext
+    {
+        // constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        //Agregar los modelos (Todos los modelos creado se colocan aqui)
+        
+    }
+}
---
+++ CrudProductos/Program.cs
@@ -0,0 +1,34 @@
+using CrudProductos.Datos;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//configuracion de la cadena de conexion 
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
